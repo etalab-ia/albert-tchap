18dd709ef50be938ef9c934edd84ac8468179e1f
Nicolas Rolin <nicolas.rolin@finances.gouv.fr>
2024-03-08 11:09:35 +0100

    add license for original simplematrixbotlib

--- src/matrix_bot/client.py
+++ src/matrix_bot/client.py
@@ -1,3 +1,4 @@
+# SPDX-FileCopyrightText: 2021 - 2022 Isaac Beverly <https://github.com/imbev>
 # SPDX-FileCopyrightText: 2023 Pôle d'Expertise de la Régulation Numérique <contact.peren@finances.gouv.fr>
 #
 # SPDX-License-Identifier: MIT
