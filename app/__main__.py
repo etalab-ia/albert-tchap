b73d98c9f276ebaf826a05186fa61a017bbbeeab
Adrien Carpentier <me@adriencarpentier.com>
2024-04-16 19:26:30 +0200

    Revert "refactor: move bot.py to main.py"
    This reverts commit 198b232e9e273fa60cf3068c4bc2cfd06f0992c7.
    
    # Conflicts:
    #	app/__main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -1,20 +1,8 @@
-from matrix_bot.bot import MatrixBot
-from commands import command_registry
-from config import env_config
-from matrix_bot.config import logger
+# SPDX-FileCopyrightText: 2023 Pôle d'Expertise de la Régulation Numérique <contact.peren@finances.gouv.fr>
+#
+# SPDX-License-Identifier: MIT
 
+from bot import main
 
 if __name__ == "__main__":
-    tchap_bot = MatrixBot(
-        env_config.matrix_home_server,
-        env_config.matrix_bot_username,
-        env_config.matrix_bot_password,
-    )
-    for callback in [
-        feature
-        for feature_group in env_config.groups_used
-        for feature in command_registry.activate_and_retrieve_group(feature_group)
-    ]:
-        logger.info("loaded feature", feature=callback.__name__)
-        tchap_bot.callbacks.register_on_message_event(callback, tchap_bot.matrix_client)
-    tchap_bot.run()
+    main()
