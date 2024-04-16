c1548246ffa8c5c6e9bffaf51922d4c148a1825e
Adrien Carpentier <me@adriencarpentier.com>
2024-04-16 11:09:03 +0200

    feat: removal of useless commands "fomo" and "kick" and add command "reset" to reset the Alert stream

--- app/commands.py
+++ app/commands.py
@@ -64,7 +64,7 @@ async def bot_help(room: MatrixRoom, message: Event, matrix_client: MatrixClient
         room=room, event=message, matrix_client=matrix_client
     )
     event_parser.do_not_accept_own_message()
-    help_message = "les commandes sont :\n - " + "\n - ".join(
+    help_message = "Les commandes sont :\n - " + "\n - ".join(
         command_registry.get_help()
     )
     event_parser.command("help", prefix="!")
@@ -86,80 +86,22 @@ async def heure(room: MatrixRoom, message: Event, matrix_client: MatrixClient):
     logger.info("Handling command", command="heure")
     await matrix_client.room_typing(room.room_id)
     await matrix_client.send_text_message(room.room_id, heure)
+    
 
-
-@register_feature(
-    help=(
-        "**!fomo [user]**: *Fear Of Missing Out*. "
-        "Donne la liste des salons où l'[user] n'est pas. Si user n'est pas précisé c'est soi-même"
-    ),
-    group="room_utils",
-)
+@register_feature(help="**!reset**: recommence un stream avec Albert", group="basic")
 @properly_fail
 @ignore_when_not_concerned
-async def fomo(room: MatrixRoom, message: Event, matrix_client: MatrixClient):
-    """fomo : Fear Of Missing Out. List the channels where the user isn't"""
+async def reset(room: MatrixRoom, message: Event, matrix_client: MatrixClient):
     event_parser = MessageEventParser(
-        room=room, event=message, matrix_client=matrix_client, log_usage=True
+        room=room, event=message, matrix_client=matrix_client
     )
     event_parser.do_not_accept_own_message()
-    missing_out_user = user_name_to_non_hl_user(
-        event_parser.command("fomo").strip() or event_parser.sender_username()
-    )
-    salons = matrix_client.get_non_private_rooms()
-    missing_out_salons = {
-        salon_item.name: get_salon_moderators(
-            salon_item, fomo_user_name=missing_out_user
-        )
-        for salon_item in salons.values()
-    }
-    message = (
-        f"Les salons **géniaux** que {missing_out_user} loupe sont :\n - "
-        + "\n - ".join(
-            [
-                f"{salon_name} : demande à {users_print(salon_moderators)}"
-                for salon_name, salon_moderators in missing_out_salons.items()
-                if salon_moderators is not None
-            ]
-        )
-    )
-    await matrix_client.send_markdown_message(room.room_id, message)
-
-
-@register_feature(
-    help=(
-        "**!kick [user]**: Donne la liste des salons où l'utilisateur est, "
-        "ainsi que la liste des gens pouvant l'expulser"
-    ),
-    group="room_utils",
-)
-@properly_fail
-@ignore_when_not_concerned
-async def kick(room: MatrixRoom, message: Event, matrix_client: MatrixClient):
-    event_parser = MessageEventParser(
-        room=room, event=message, matrix_client=matrix_client, log_usage=True
-    )
-    event_parser.do_not_accept_own_message()
-    maybe_user_name = user_name_to_non_hl_user(
-        event_parser.command("kick").strip() or event_parser.sender_username()
-    )
-
-    salons = matrix_client.get_non_private_rooms()
-    kick_out_salons = {
-        salon_item.name: get_salon_moderators(
-            salon_item, kick_user_name=maybe_user_name
-        )
-        for salon_item in salons.values()
-    }
-    maybe_user_name = f"**{maybe_user_name}**"
-    message = f"Pour expulser {maybe_user_name} :\n - " + "\n - ".join(
-        [
-            f"{salon_name} : demande à {users_print(salon_moderators) or maybe_user_name}"
-            for salon_name, salon_moderators in kick_out_salons.items()
-            if salon_moderators is not None
-        ]
-    )
-    await matrix_client.send_markdown_message(room.room_id, message)
+    event_parser.command("reset", prefix="!")
+    #TODO: Albert reset stream
+    reset: str = "La conversation a été remise à zéro."
+    logger.info("Handling command", command="reset")
+    await matrix_client.room_typing(room.room_id)
+    await matrix_client.send_text_message(room.room_id, reset)
 
 
 @register_feature(
@@ -180,12 +122,15 @@ async def bot_albert(room: MatrixRoom, message: Event, matrix_client: MatrixClie
     await matrix_client.room_typing(room.room_id, typing_state=True, timeout=180_000)
     if env_config.albert_api_url and env_config.albert_token:
         # TODO: send the prompt as a HTTP request to Albert and get the response
+        await matrix_client.send_text_message(
+            room.room_id, "Pour remettre à zéro la conversation, tapez `!reset`"
+        )  # TODO
         pass
     else:
-        ALBERT_ERROR_REPONSE = "Albert n'est pas configuré. Contactez albert-contact@data.gouv.fr pour signaler cette erreur."
-    logger.info(f"{ALBERT_ERROR_REPONSE=}")
+        ALBERT_ERROR_RESPONSE = "Albert n'est pas configuré. Contactez albert-contact@data.gouv.fr pour signaler cette erreur."
+    logger.info(f"{ALBERT_ERROR_RESPONSE=}")
     try:  # sometimes the async code fail (when input is big) with random asyncio errors
-        await matrix_client.send_text_message(room.room_id, ALBERT_ERROR_REPONSE)
+        await matrix_client.send_text_message(room.room_id, ALBERT_ERROR_RESPONSE)
     except Exception as llm_exception:  # it seems to work when we retry
         logger.warning(f"Albert response failed with {llm_exception=}. retrying")
-        await matrix_client.send_text_message(room.room_id, ALBERT_ERROR_REPONSE)
+        await matrix_client.send_text_message(room.room_id, ALBERT_ERROR_RESPONSE)
